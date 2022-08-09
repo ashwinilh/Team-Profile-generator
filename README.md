a998361f62b8385b3e5c3a09ecbd900ca4b4b49c
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-08-08 22:18:56 -0500

    added readme file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Team-Profile-generator
+![screen-shot-challenge11](./images/Screenshot-1.jpg)
+
+1. Created a repo "Team-Profile-generator" https://github.com/ashwinilh/Team-Profile-generator
+2. This repo is for challenge 10 of UT boot camp to  build a Node.js command-line application that takes in information about employees on a software engineering team and generates an HTML webpage that displays summaries for each person
+3.  Created folder structure
+4. Addwed initial code.
+5. Added employee class, then Manager, Engineer and Intern class.
+6. Added inquirer questions to generate team and generated html data.
+7. Added code for write file
+8. Added validation for email,ID and office number
+9. Added prompt questions.
---
+++ app.js
@@ -0,0 +1,143 @@
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const questions = require("./lib/questions");
+
+const RESULT_DIR = path.resolve(__dirname, "results");
+const outputPath = path.join(RESULT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+//Array to hold all team members
+let team = [];
+
+
+// Function to create manager and build team based on manager's input
+function buildManager() {
+  try {
+    inquirer.prompt(questions.managerQuestions).then(function (answers) {
+      //  creates manager object and pushes manager data in team array
+      const manager = new Manager(
+        answers.manager_name,
+        answers.manager_id,
+        answers.manager_email,
+        answers.manager_officeNo
+      );
+      team.push(manager);
+
+      // function called to build team
+      buildTeam();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+// function to build team
+async function buildTeam() {
+  while (true) {
+    await inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "team_member",
+          message: "What type of team member would you like to add?",
+          choices: [
+            "Engineer",
+            "Intern",
+            "I don't want to add any more team members.",
+          ],
+        },
+      ])
+      .then(async function (response) {
+        // If manager selects to include 'Engineer'
+        if (response.team_member === "Engineer") {
+          try {
+            await inquirer
+              .prompt(questions.engineerQuestions)
+              .then(function (engineerData) {
+                // creates engineer object from manager's response and push into team array
+                const engineer = new Engineer(
+                  engineerData.engineer_name,
+                  engineerData.engineer_id,
+                  engineerData.engineer_email,
+                  engineerData.engineer_github
+                );
+
+                team.push(engineer);
+              });
+          } catch (error) {
+            console.log(error);
+          }
+
+          // If manager selects to include 'Intern'
+        } else if (response.team_member === "Intern") {
+          
+          try {
+          
+            await inquirer.prompt(questions.internQuestions).then(function (internData) {
+          
+              //creates intern object from manager's response and push into team array
+              const intern = new Intern(
+                internData.intern_name,
+                internData.intern_id,
+                internData.intern_email,
+                internData.intern_school
+              );
+
+              team.push(intern);
+            
+            });
+          } catch (error) {
+            console.log(error);
+          }
+
+          // If manager done with team
+        } else if (
+          response.team_member === "I don't want to add any more team members."
+        ) {
+          // called render frunction by passing team array and htmldata is obtained from that function
+          const htmlData = render(team);
+
+          // Function called to write to file and generate html output file
+          writeToFile(htmlData);
+
+          // To exit from process and while loop
+          process.exit(0);
+        }
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+}
+
+
+// Function to write to output file
+function writeToFile(htmlData) {
+
+  // Checks if 'output' folder exists in current directory
+  if (!fs.existsSync(RESULT_DIR)) {
+
+    // Create 'output' folder if does not exists
+    fs.mkdirSync(RESULT_DIR, 0744);
+    console.log("output folder Created!");
+
+  }
+
+  // write htmldata to 'team.html' file
+  fs.writeFileSync(outputPath, htmlData, "utf8");
+
+  console.log("Output File generated!");
+}
+
+function init() {
+  console.log("Welcome to Team Profile Generator! Build your team :");
+  buildManager();
+}
+
+init();
